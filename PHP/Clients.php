2f4a01c9d6ef78a45f9965fd8772246dd21f322c
unknown <brandon.da-silva@moniris.com>
2018-12-18 10:33:43 +0100

    Update details

--- PHP/Clients.php
+++ PHP/Clients.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Clients</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
@@ -20,7 +20,6 @@
         {
             echo "<p>".$ligne['nom']." - " .$ligne['prenom']." - ".$ligne['ancienReleve']." - " .$ligne['dernierReleve']. "</p>";
         }
-
     ?>
 </body>
 </html>
