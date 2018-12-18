3d59e2f7a41651ec591ec9eb12c69f5366abf9db
unknown <brandon.da-silva@moniris.com>
2018-12-18 09:48:02 +0100

    new

---
+++ PHP/Clients.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <?php
+
+        include 'cnx.php';
+        $sql = $cnx->prepare("select nom,prenom,ancienReleve,dernierReleve from client");
+        $sql->execute();
+
+        foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+        {
+            echo "<p>".$ligne['nom']." - " .$ligne['prenom']." - ".$ligne['ancienReleve']." - " .$ligne['dernierReleve']. "</p>";
+        }
+
+    ?>
+</body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Controleurs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <?php
+        include 'C:\xampp\htdocs\Master2019\DevoirEDF\PHP\cnx.php';
+        $sql = $cnx->prepare("select id, nom, prenom from controleur");
+        $sql->execute();
+        echo "<table border='1'>";
+        foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+        {
+            echo "<tr>";
+                echo "<td>".$ligne['nom']."</td>";
+                echo "<td>".$ligne['prenom']."</td>";
+                echo "<td><a href='Clients.php?controleur".$ligne['id']."'>Tous les clients</a></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+
+    ?>
+</body>
+</html>
